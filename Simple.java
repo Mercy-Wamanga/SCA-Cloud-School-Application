1babd59e6e6d20793a6df33e2e7873994243453d
Mercy Wamanga <tashma19@gmail.com>
2021-07-20 23:54:23 +0000

    update3

--- Simple.java
+++ Simple.java
@@ -1,7 +1,10 @@
 public class Simple{  
     public static void main(String[] args){  
      System.out.println("Hello Java");  
-     System.out.println("Welcome to SCA Clould SChool ");   
-     System.out.println("Welcome to SCA Clould SChool ");  
+     System.out.println("Welcome to SCA Clould SChool2 ");   
+     System.out.println("Welcome to SCA Clould SChool3 ");  
+     System.out.println("Welcome to SCA Clould SChool10 "); 
+     System.out.println("Welcome to SCA Clould SChool1"); 
+
     }  
 }  
