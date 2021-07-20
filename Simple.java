60a76b05f299cb59c2e0ebaa6d2e2b3cb7de3c90
Mercy Wamanga <tashma19@gmail.com>
2021-07-20 23:13:03 +0000

    update- simple file

--- Simple.java
+++ Simple.java
@@ -2,5 +2,6 @@ public class Simple{
     public static void main(String[] args){  
      System.out.println("Hello Java");  
      System.out.println("Welcome to SCA Clould SChool ");   
+     System.out.println("Welcome to SCA Clould SChool ");  
     }  
 }  
